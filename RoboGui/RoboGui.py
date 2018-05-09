f1ae3a1e081d7b919cb0533fef77dfb369c0df74
Sheldon DSouza <sheldon.s.d@gmail.com>
2018-05-08 23:22:34 -0400

    Logic to create the command from the selections.

--- RoboGui/RoboGui.py
+++ RoboGui/RoboGui.py
@@ -1,4 +1,6 @@
 import sys
+import subprocess
+import os
 
 from PyQt5.QtWidgets import (QWidget,
                              QApplication,
@@ -10,7 +12,8 @@ from PyQt5.QtWidgets import (QWidget,
                              QLineEdit,
                              QCheckBox,
                              QTabWidget,
-                             QPushButton)
+                             QPushButton,
+                             QMessageBox)
 
 from CustomUI import FileChooser
 from RobocopySwitchDictionary import allOptions
@@ -20,19 +23,18 @@ class RoboGUI(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.source = ''
-        self.target = ''
 
     def initUI(self):
 
         test_button = QPushButton("Preview")
         exec_button = QPushButton("Start!!")
-        save_button = QPushButton("Save")
+        # save_button = QPushButton("Save")
 
         btnLayout = QHBoxLayout()
         btnLayout.addStretch()
-        btnLayout.addWidget(save_button)
+        # btnLayout.addWidget(save_button)
         btnLayout.addWidget(test_button)
+        test_button.clicked.connect(self.run_test)
         btnLayout.addWidget(exec_button)
 
         layout = QVBoxLayout()
@@ -46,15 +48,19 @@ class RoboGUI(QWidget):
 
     def setup_file_chooser(self):
         layout = QGridLayout()
-        layout.addWidget(FileChooser("Source", "Select Source", True), 1, 0)
-        layout.addWidget(FileChooser("Target", "Select Target", True), 2, 0)
+        source = FileChooser("Source", "Select Source", True)
+        source.setObjectName("Source Directory")
+        target = FileChooser("Target", "Select Target", True)
+        target.setObjectName("Target Directory")
+        layout.addWidget(source, 1, 0)
+        layout.addWidget(target, 2, 0)
         return layout
 
     def setup_copy_options(self):
         tabWidget = QTabWidget()
         layout = QHBoxLayout()
         layout.addWidget(tabWidget)
-        tabs = ["Source Options", "Destination Options", "Copy Options", "Logging options"]
+        tabs = ["Source", "Destination", "Copy", "Logging", "Advanced"]
         for tab in tabs:
             tabWidget.addTab(self.setup_ui_from_options(tab), tab)
         return layout
@@ -80,11 +86,13 @@ class RoboGUI(QWidget):
                 container.setSpacing(5)
                 container.addWidget(QLabel(key_options[option]))
                 entrygrid.addLayout(container, entry, 0)
+                option = option[:option.find(":") + 1]  # Strip directives
                 entry += 1
             elif option.find(":file") > 0:
                 # File Browser
-                widget = FileChooser(key_options[option], key_options[option], False, True)
+                widget = FileChooser(key_options[option], key_options[option], False)
                 entrygrid.addWidget(widget, entry, 0)
+                option = option[:option.find(":") + 1]  # Strip directives
                 entry += 1
             else:
                 # Check box
@@ -93,7 +101,7 @@ class RoboGUI(QWidget):
                 check += 1
 
             widget.setProperty("option", option)
-            widget.setProperty("robocopy_flag", True)
+            widget.setObjectName("option")
 
         panel = QWidget()
         grid = QVBoxLayout()
@@ -103,16 +111,69 @@ class RoboGUI(QWidget):
         panel.setLayout(grid)
         return panel
 
+    def run_test(self):
+        command = self.get_parameters(self)
+        command += " /L"
+        # subprocess.check_call(command, shell=False)
+        # os.system("start /wait cmd /c {%s}" % command)
+        # subprocess.Popen("start /wait cmd /c {%s}" % command).wait()
+
+    def get_parameters(self, parent):
+        command = 'cmd.exe robocopy '
+        source = self.get_path("Source Directory", parent)
+        if not source:
+            return
+        command = command + " " + source
+        target = self.get_path("Target Directory", parent)
+        if not target:
+            return
+        command = command + " " + target
+
+        children = parent.findChildren(QWidget, name="option")
+        for child in children:
+            # Checkbox
+            if type(child) is QCheckBox:
+                if child.isChecked():
+                    command = command + " " + child.property("option")
+            # File Chooser
+            elif type(child) is FileChooser:
+                if child.getSelection() != '':
+                    command = command + " " + \
+                              child.property("option") + child.getSelection()
+            # Text box
+            elif type(child) is QLineEdit:
+                if child.text() != '':
+                    command = command + " " + \
+                              child.property("option") + child.text()
+        print("%s" % command)
+        return command
+
+    @staticmethod
+    def get_path(name, container):
+        widget = container.findChildren(FileChooser, name=name)
+        path = widget[0].getSelection()
+        if not path:
+            error = str("%s is not specified" % name)
+        # elif QDir.exists(path.re):
+        #     error = str("%s is not a valid location" % name)
+        else:
+            return path
+
+        if error:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(error)
+            msg.setInformativeText("In order to continue, please correct this issue and retry.")
+            msg.setWindowTitle("Cannot proceed!")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec()
+
+        return False
+
 
 if __name__ == '__main__':
-    #print("%s" % str(options.allOptions))
+    # print("%s" % str(options.allOptions))
     app = QApplication(sys.argv)
     app.setStyle(QStyleFactory.create("GTK+"))
     ex = RoboGUI()
     sys.exit(app.exec_())
-
-# ly = panel.layout()
-# for i in range(ly.count()):
-#     item = ly.itemAt(i)
-#     item = item.widget()
-#     print("linedit: %s  - %s\n" % (item.property("option"), item.text()))
