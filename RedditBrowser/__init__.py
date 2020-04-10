6d1a478d4df4d628cbf8d63add43721e136ec8dd
Sheldon DSouza <sheldon.s.d@gmail.com>
2020-04-10 17:24:56 -0400

    Initial Setup of FileWrangler and RedditBrowser

---
+++ RedditBrowser/__init__.py
@@ -0,0 +1,8 @@
+
+from common import CommonUtils
+
+__VERSION__ = "0.0.0"
+__NAME__ = "RedditBrowser"
+__APP_NAME__ = str.format(f"{__NAME__} {__VERSION__}")
+
+logger = CommonUtils.get_logger(__APP_NAME__)
